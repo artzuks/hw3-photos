db16fcfc40416fb4cea6d9a9ecb4f8acd9453ad5
Zuks <azuks1@bloomberg.com>
2018-11-23 13:54:54 -0500

    added font awesome

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+
+    <Main/>
+    <Upload/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Main from '@/components/Main.vue'
+import Upload from '@/components/Upload.vue'
+export default {
+  name: 'home',
+  components: {
+    Main,
+    Upload
+  }
+}
+</script>
